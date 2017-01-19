9c4df6ed5836b39082080efb3965cc27c8843a98
Jace <chesszhu@gmail.com>
2017-01-18 16:40:43 -0800

    lesson 6

--- x1.py
+++ x1.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+# lesson 4
+
 cars = 5
 
 print 'there are', cars, 'cars.'
@@ -11,3 +13,10 @@ size = 10
 
 print 'there is %s trees.' % tree
 print 'it\'s %d stories tall and %s million dollars' % (size, tree)
+
+#lesson 6
+
+x = 'there are %d types of people' % 10
+binary = 'binary'
+do_not = "don't"
+y = 'those who know %s and those who %s' % (binary, do_not)
